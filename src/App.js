07a099760ae6c7213442274230c7cf3f070afe43
swapnilsparsh <arezona.lucky55@gmail.com>
2021-07-30 23:47:49 +0530

    Button Styling

--- src/App.js
+++ src/App.js
@@ -105,7 +105,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-      <button type="submit" disabled={!formValue}>🕊️</button>
+      <button type="submit" disabled={!formValue}>📩</button>
 
     </form>
   </>)
